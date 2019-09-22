98cf64ab84d648ee803a44a1e785b4e947063ea1
Guayo Mena <guayo.mena@gmail.com>
2019-09-21 23:08:35 -0600

    Fix(styles): Add margin to footer links

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
 import Box from '@material-ui/core/Box'
-
 import Link from '@material-ui/core/Link'
+import { styled } from '@material-ui/styles'
+
+const FooterLink = styled(Link)({
+  margin: 4,
+})
 
 export default function Footer() {
   return (
@@ -13,16 +16,11 @@ export default function Footer() {
       alignItems="center"
       p={4}
     >
-      <Box component="nav" display="flex" my={2} alignItems="center">
-        <Link href="https://github.com/reactcostarica">GitHub</Link>
-        <Box
-          mx={2}
-          width={5}
-          height={5}
-          bgcolor="secondary"
-          borderRadius="50%"
-        />
-        <Link href="https://twitter.com/reactcostarica">Twitter</Link>
+      <Box component="nav" display="flex" alignItems="center">
+        <FooterLink href="https://github.com/reactcostarica">GitHub</FooterLink>
+        <FooterLink href="https://twitter.com/reactcostarica">
+          Twitter
+        </FooterLink>
       </Box>
 
       <Link color="textSecondary" href="mailto:hola@reactcostarica.com">
